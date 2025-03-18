11b604d268f39d2f83550c672bc3c60f383aca04
jeancharlesx64 <jean.albuquerque@sptech.school>
2025-03-18 12:54:15 -0300

    [FEAT] :: fixing the versioning of swagger doc

--- src/main/java/br/com/exemplo/backendapitemplate/v1/controller/UserController.java
+++ src/main/java/br/com/exemplo/backendapitemplate/v1/controller/UserController.java
@@ -2,6 +2,8 @@ package br.com.exemplo.backendapitemplate.v1.controller;
 
 import br.com.exemplo.backendapitemplate.v1.model.User;
 import br.com.exemplo.backendapitemplate.v1.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +15,12 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v1/users")
+@Tag(name = "Users API - v1", description = "Gerenciamento de usuários | Endpoints")
 public class UserController {
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "Buscar todos os usuários", description = "Retorna uma lista com todos os usuários cadastrados na base de dados")
     @GetMapping
     public ResponseEntity<?> getAllUsers() {
         try{
@@ -29,6 +33,7 @@ public class UserController {
         }
     }
 
+    @Operation(summary = "Buscar usuário pelo ID", description = "Retorna apenas um usuário encontrado pelo ID na base de dados")
     @GetMapping("/{id}")
     public ResponseEntity<?> getUserById(@PathVariable long id) {
         try{
@@ -43,6 +48,7 @@ public class UserController {
         }
     }
 
+    @Operation(summary = "Registrar um usuário", description = "Registra um usuário na base de dados")
     @PostMapping
     public ResponseEntity<?> createUser(@RequestBody User user) {
         try{
@@ -59,6 +65,7 @@ public class UserController {
         }
     }
 
+    @Operation(summary = "Atualizar um usuário pelo ID", description = "Atualiza os dados de um usuário através do ID encontrado na base de dados")
     @PutMapping("/{id}")
     public ResponseEntity<?> updateUser(@PathVariable long id, @RequestBody User user) {
         try{
@@ -76,6 +83,7 @@ public class UserController {
         }
     }
 
+    @Operation(summary = "Deletar um usuário pelo ID", description = "Deleta um usuário pelo ID do usuário encontrado na base de dados")
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteUser(@PathVariable long id) {
         try{
